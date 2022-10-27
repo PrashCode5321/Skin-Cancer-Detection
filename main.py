0fe0cedfd18933020e4e341454b13e7b23922d54
RishabhRishiRishu <rishabhrishirishu@gmail.com>
2022-10-27 16:27:47 +0530

    .

---
+++ main.py
@@ -0,0 +1,19 @@
+# Input Image of skin Lesion
+
+# Contrast Enhancement
+
+# Thresholding in RGB planes
+
+# Creating binary masks using thresholding
+
+# Find the largest blob
+
+# Edge Detection
+
+# Segmented image containing only the lesion
+
+# Extracting the geometry features
+
+# Classification
+
+# Result
